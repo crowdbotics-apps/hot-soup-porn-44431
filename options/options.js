6318015346a55ab1468d49fa01dbb4b2b2c9780f
Crowdbotics <team@crowdbotics.com>
2023-11-09 01:21:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hot-soup-porn-44431",
+  url: "https://hot-soup-porn-44431.botics.co",
+  api: "https://hot-soup-porn-44431.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
